4e177105a31e92549b8d471e2945f77e60d40633
Zifeng Lu <lukforce@gmail.com>
2023-12-08 12:26:36 -0800

    [General] Added resource update script to repo

---
+++ vendor/optc-db/updateResources.sh
@@ -0,0 +1,6 @@
+wget -N https://raw.githubusercontent.com/optc-db/optc-db.github.io/master/common/data/units.js
+wget -N https://raw.githubusercontent.com/optc-db/optc-db.github.io/master/common/data/details.js
+wget -N https://raw.githubusercontent.com/optc-db/optc-db.github.io/master/common/data/evolutions.js
+wget -N https://raw.githubusercontent.com/optc-db/optc-db.github.io/master/common/data/flags.js
+wget -N https://raw.githubusercontent.com/optc-db/optc-db.github.io/master/common/data/cooldowns.js
+wget -N https://raw.githubusercontent.com/optc-db/optc-db.github.io/master/common/data/families.js
