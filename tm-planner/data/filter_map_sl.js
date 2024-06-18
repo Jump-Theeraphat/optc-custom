f5a04f956dc853987680e26a0e6e042b7835d54c
Zifeng Lu <lukforce@gmail.com>
2024-06-17 19:20:26 -0700

    [TM Planner] Removed unused files

--- tm-planner/data/filter_map_sl.js
+++
@@ -1,8 +0,0 @@
-var filter_map_sl = {
-    'slot-pos':                 /makes ([^".]+?)orbs beneficial for ([^".]+?)characters?/i,
-    'cd-red':                   /restores.+special cooldowns?.+rewinded/i,
-    'sp-bind-red':              /(?:reduces|removes)[^."]+?silence[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?: on ([^."]+?)characters?)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i,
-    'para-red':                 /(?:reduces|removes)[^."]+?paralysis[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?: on ([^."]+?)characters?)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i,
-    'blind-red':                /(?:reduces|removes)[^."]+?(?:blindness|selected debuffs?)[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i,
-    'burn-red':                 /(?:reduces|removes)[^."]+?(?:Burn|selected debuffs?)[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i
-};
--- tm-planner/data/filter_map_sup.js
+++
@@ -1,5 +0,0 @@
-var filter_map_sup = {
-    'slot-change':              /change.*orb/i,
-    'slot-change-block':        /change.*block/i,
-    'bind-red':                 /(?:reduces|removes)(?: |[^."]+?, |[^."]+? and )bind[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))( (?:for|on) the supported character)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i
-};
