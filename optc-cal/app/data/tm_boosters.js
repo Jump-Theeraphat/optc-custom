be32bd23aa1087dcbd0b5b0ba3d2189546c35df9
Zifeng Lu <lukforce@gmail.com>
2025-10-21 16:44:01 -0700

    [Calendar] TM booster list reset

--- optc-cal/app/data/tm_boosters.js
+++ optc-cal/app/data/tm_boosters.js
@@ -1,6 +1,5 @@
 var tm_boosters = [
     // TMs
-    4414,
     4430,
     4448,
 
@@ -9,10 +8,8 @@ var tm_boosters = [
     // Coliseums
 
     // Kizunas
-    4439,
 
     // PKAs
-    4447,
 
     // Events
 ]
