aeb260e57255b02110539b80ffef16f0397c8a97
Zifeng Lu <lukforce@gmail.com>
2025-11-09 01:40:27 -0800

    [Guide Converter] Bug fixes - Fixed Not Translated converter - Fixed regex

--- guide-converter/assets/js/guide-converter.js
+++ guide-converter/assets/js/guide-converter.js
@@ -8,8 +8,11 @@ $(document).ready(function () {
             guideConverted = guideConverted.replace(regex, replacement);
         });
 
+        // Remove empty lines
+        guideConverted = guideConverted.replace(/^$\n/gm, "");
+
         // Not translated
-        guideConverted = guideConverted.replace(/\n([^\{].*[^\}])\n/, "\n\"Not Translated: $1\",\n");
+        guideConverted = guideConverted.replace(/^(\n?)([^\{].+[^\}])(\n?)$/gm, "$1\"Not Translated: $2\",$3");
 
         // Remove last comma to make valid JSON
         guideConverted = guideConverted.replace(/,\s*$/, "");
@@ -18,12 +21,9 @@ $(document).ready(function () {
 
         $('#guide-converted').val(guideConverted);
 
-        var guideStageTypeClone = $('#guide-stage-type');
-        var guideJson = JSON.parse(guideConverted, (key, value) => {
-            console.log(`key: ${key}, value: ${value}`);
-            return typeof key === undefined ? 'Not Translated' : value;
-        });
-        decorateGuideAction(guideJson, 'guide-converter', guideStageTypeClone, -1);
+        var guideStageTypeClone = $('#guide-stage-type-clone').clone();
+        guideStageTypeClone.attr('id', 'guide-converter-stage-type');
+        decorateGuideAction(JSON.parse(guideConverted), 'guide-converter', guideStageTypeClone, -1);
 
         $('#guide-display-div').empty();
         $('#guide-display-div').append(guideStageTypeClone);
